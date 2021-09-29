a6741285520cbc27ac761ba35f469f42fbe8d6ba
Monse-Arzate <monserrat.arzate@fi.unam.edu>
2021-09-28 20:08:32 -0500

    SE dividio en funciones

--- cbd.c
+++ cbd.c
@@ -3,18 +3,26 @@
 #include<math.h>
 #include<string.h>
 
+int  conv_binario_decimal(char *binario[]);
+
 int main(){
-    int num = 0, pot = 0;
+    int num;
     char binario[100];
 
     scanf("%s", &binario);
+    num = conv_binario_decimal(binario);
 
+    printf("%d\n", num);
+    return 0; 
+}
+
+int  conv_binario_decimal(char *binario[])
+{
+    int num = 0, pot = 0;
     for(int i = strlen(binario)-1; i>=0; i--){
         if(binario[i]=='1') 
             num= num + pow(2, pot);
         pot = pot + 1;
-    }
-
-    printf("%d\n", num);
-    return 0;
-}
+    } 
+    return num;
+    
